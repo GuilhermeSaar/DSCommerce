4b2dd073a9163c76374629019551d06718d532aa
Guilherme Saar <guilhermesaar02@gmail.com>
2024-11-07 18:25:30 -0300

    create userDetailsService

---
+++ src/main/java/com/devsuperior/dscommerce/projection/UserDetailsProjection.java
@@ -0,0 +1,10 @@
+package com.devsuperior.dscommerce.projection;
+
+public interface UserDetailsProjection {
+
+    String getUserName();
+    String getPassword();
+    Long getRoleId();
+    String getAuthority();
+
+}
